d6b1f93bc251b442f669376ce51a35bb82d19c50
Edward Evans <elevans2@wisc.edu>
2024-04-23 13:54:06 -0500

    Add init file to initialize Op environment

---
+++ src/pyops/__init__.py
@@ -0,0 +1,16 @@
+import scyjava as sj
+
+def init():
+    """Initialize a SciJava Ops environment.
+    """
+    # add SciJava repository
+    sj.config.add_repositories({"scijava.public': 'https://maven.scijava.org/content/groups/public"})
+
+    # add endpoints
+    sj.config.endpoints = ["net.imglib2:imglib2",
+            "net.imglib2:imglib2-imglyb",
+            "io.scif:scifio",
+            "org.scijava:scijava-ops-engine:0-SNAPSHOT",
+            "org.scijava:scijava-ops-image:0-SNAPSHOT"]
+
+    return sj.jimport("org.scijava.ops.api.OpEnvironment").build()
