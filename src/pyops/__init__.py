943c6c31dbd9f3efd85fae2387de0d1f4516b5f9
Edward Evans <elevans2@wisc.edu>
2024-04-24 10:03:04 -0500

    Seperate JVM creation and Ops env

--- src/pyops/__init__.py
+++ src/pyops/__init__.py
@@ -1,16 +1,31 @@
 import scyjava as sj
+from typing import Sequence
 
-def init():
-    """Initialize a SciJava Ops environment.
+def get_ops():
+    """Get an instance of the Ops Environment.
+    """
+    if not sj.jvm_started():
+        init()
+
+    return sj.jimport("org.scijava.ops.api.OpEnvironment").build()
+
+
+def init(endpoints: Sequence[str]=None):
+    """Configure and start the JVM with SciJava Ops
+
+    :param endpoints: A list or tuple of endpoint strings
     """
     # add SciJava repository
     sj.config.add_repositories({"scijava.public": "https://maven.scijava.org/content/groups/public"})
 
     # add endpoints
-    sj.config.endpoints = ["net.imglib2:imglib2",
-            "net.imglib2:imglib2-imglyb",
-            "io.scif:scifio",
-            "org.scijava:scijava-ops-engine:0-SNAPSHOT",
-            "org.scijava:scijava-ops-image:0-SNAPSHOT"]
+    if not endpoints:
+        sj.config.endpoints = ["net.imglib2:imglib2",
+                "net.imglib2:imglib2-imglyb",
+                "io.scif:scifio",
+                "org.scijava:scijava-ops-engine:0-SNAPSHOT",
+                "org.scijava:scijava-ops-flim:0-SNAPSHOT",
+                "org.scijava:scijava-ops-image:0-SNAPSHOT"]
 
-    return sj.jimport("org.scijava.ops.api.OpEnvironment").build()
+    # start the JVM
+    sj.start_jvm()
